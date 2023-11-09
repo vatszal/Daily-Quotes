05f5eaefbaf22d95c26f89e19d34748743f76eff
vatszal <135651028+vatszal@users.noreply.github.com>
2023-11-09 15:01:04 +0530

    API changed, localstorage, css var

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 //quote
 const quoteText = document.getElementById("quote");
-const Url = "https://api.kanishkrawatt.tech/quotes";
+const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e1341f35c7msha10adc079f9ca91p16c715jsn8549f844ca5f',
+        'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
+    }
+};
 
+// Function to update the quote
+function updateQuote() {
+    fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+            const quote = data.text;
+            const author = data.author;
+            quoteText.innerHTML = `"${quote}" - ${author}`;
 
-fetch(Url)
-    .then((response) => response.json())
-    .then((data) => {
-        const quote = data.Quote;
-        const author = data.Author;
-        quoteText.innerHTML = `"${quote}" - ${author}`;
-    });
+            localStorage.setItem('quote', JSON.stringify({ quote, author }));
+        })
+        .catch(error => console.error('Error fetching quote:', error));
+}
+
+const storedQuote = localStorage.getItem('quote');
+
+if (storedQuote) {
+    const { quote, author } = JSON.parse(storedQuote);
+    quoteText.innerHTML = `"${quote}" - ${author}`;
+} else {
+    // If there's no stored quote, fetch a new one and store it
+    updateQuote();
+}
+
+// Set up a setInterval to update the quote every 24 hours (in milliseconds)
+setInterval(updateQuote, 24 * 60 * 60 * 1000);
+
 
 
 //clock
@@ -35,3 +61,4 @@ const showTime= ()=>{
 
     timeformat.innerHTML = hr>12 ? "PM" : "AM";
 }
+
