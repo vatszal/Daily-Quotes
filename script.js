e042c44e91a2c7fe6c97027400f348947d9026d3
vatszal <135651028+vatszal@users.noreply.github.com>
2023-11-09 11:29:04 +0530

    add blacj

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//quote
 const quoteText = document.getElementById("quote");
 const Url = "https://api.kanishkrawatt.tech/quotes";
 
@@ -11,5 +12,26 @@ fetch(Url)
     });
 
 
+//clock
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformat');
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    setInterval(showTime, 1000);
+});
 
+const showTime= ()=>{
+    let date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let secs = date.getSeconds();
+
+    hr = hr<10 ? `0${hr}` : hr;
+    min = min<10 ? `0${min}` : min;
+    secs = secs<10 ? `0${secs}` : secs;
+
+    time.innerHTML = `${hr} : ${min} : ${secs}`;
+
+    timeformat.innerHTML = hr>12 ? "PM" : "AM";
+}
